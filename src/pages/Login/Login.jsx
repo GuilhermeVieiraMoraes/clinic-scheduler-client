936198420a90655b345c98de26321fb2f81376d7
GuilhermeVieiraMoraes <guims02@hotmail.com>
2026-02-05 17:10:32 -0300

    style: add image to login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -12,24 +12,28 @@ const Login = () => {
   }
 
   return (
-    <div className={styles.login_card}>
-      <h1>Bem-vindo de volta</h1>
-      <p>Agende suas consultas com facilidade e rapidez.</p>
-      <div className={styles.user_type}>
-        <button>Cliente</button>
-        <button>Administrador</button>
+    <div className={styles.login_container}>
+      <div className={styles.img_container}><img src="src/assets/clinic.jpg" alt="clinica" /></div>
+      <div className={styles.login_card}>
+        <h1>Bem-vindo de volta</h1>
+        <p>Agende suas consultas com facilidade e rapidez.</p>
+        <div className={styles.user_type}>
+          <button>Cliente</button>
+          <button>Administrador</button>
+        </div>
+        <form className={styles.login_form} onSubmit={handleSubmit}>
+          <label>
+            <span>Username</span>
+            <input type="text" placeholder="Insert username" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+          </label>
+          <label>
+            <span>Password</span>
+            <input type="password" placeholder="Insert password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
+          </label>
+          <button>Login</button>
+        </form>
+        <p>Não tem uma conta? <a href="/">Cadastre-se gratuitamente</a></p>
       </div>
-      <form className={styles.login_form} onSubmit={handleSubmit}>
-        <label>
-          <span>Username</span>
-          <input type="text" placeholder="Insert username" value={username} onChange={(e) => setUsername(e.target.value)} required/>
-        </label>
-        <label>
-          <span>Password</span>
-          <input type="password" placeholder="Insert password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
-        </label>
-        <button>Login</button>
-      </form>
     </div>
   )
 }
