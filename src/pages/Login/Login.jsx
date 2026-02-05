58f633ce665c9c405ae6fbc92a1b198baa001c09
GuilhermeVieiraMoraes <guims02@hotmail.com>
2026-02-05 00:47:03 -0300

    style: add login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,36 @@
+import styles from './Login.module.css'
 
+import { useState } from "react"
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
-    <div>Login</div>
+    <div className={styles.login_card}>
+      <h1>Bem-vindo de volta</h1>
+      <p>Agende suas consultas com facilidade e rapidez.</p>
+      <div className={styles.user_type}>
+        <button>Cliente</button>
+        <button>Administrador</button>
+      </div>
+      <form className={styles.login_form} onSubmit={handleSubmit}>
+        <label>
+          <span>Username</span>
+          <input type="text" placeholder="Insert username" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="password" placeholder="Insert password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
+        </label>
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
 
